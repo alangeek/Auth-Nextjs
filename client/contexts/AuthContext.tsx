79cec1d19c373a0c5742d0c3f0a3b95b431f55e4
Alan Christian <alanchristiancreator@hotmail.com>
2021-08-22 18:40:41 -0300

    Refresh Token

--- client/contexts/AuthContext.tsx
+++ client/contexts/AuthContext.tsx
@@ -36,7 +36,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
     if (token) {
       api.get('/me').then(response => {
-        const { email, permissions, roles } = response.data
+        const { email, permissions, roles } = response.data;
 
         setUser({ email, permissions, roles })
       })
